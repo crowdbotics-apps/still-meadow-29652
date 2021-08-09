f4e61fe79452ec11120100c9b773c500129acad8
Crowdbotics <team@crowdbotics.com>
2021-08-09 02:52:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "still_meadow_29652Identifier",
+  url: "https://still_meadow_29652Identifier.botics.co",
+  api: "https://still_meadow_29652Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
